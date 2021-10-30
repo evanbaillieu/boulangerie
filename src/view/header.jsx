79a5ca99626701fc583b7f94af506366fbf163fb
evanbaillieu <baillieu.evan@gmail.com>
2021-10-30 15:59:21 +0200

    create Header

---
+++ src/view/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <ul>
+                <li>encre &amp; levure</li>
+                <li>patiserie</li>
+                <li>cours de patiserie</li>
+                <li>a propos</li>
+            </ul>
+        </header>
+    );
+}
+
+export default Header;
